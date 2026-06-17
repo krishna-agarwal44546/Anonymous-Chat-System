8d21ddf3185b369bf431a260d286b367e63b823f
krishna-agarwal44546 <krishnaiit7@gmail.com>
2026-06-17 18:25:30 +0530

    second commit

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import asyncio
 import os
-from http import HTTPStatus
 import websockets
 
-PORT = int(os.environ.get("PORT", 8765))
+PORT = int(os.environ.get("PORT", 10000))
 
 async def echo(websocket):
-    async for message in websocket:
-        print(f"Received: {message}")
-        await websocket.send(f"Echo: {message}")
+    print("Client connected")
 
-def process_request(connection, request):
-    return connection.respond(
-        HTTPStatus.OK,
-        "WebSocket server is running\n"
-    )
+    async for message in websocket:
+        print("Received:", message)
+        await websocket.send(f"Echo: {message}")
 
 async def main():
     async with websockets.serve(
         echo,
         "0.0.0.0",
-        PORT,
-        process_request=process_request
+        PORT
     ):
         print(f"WebSocket server running on port {PORT}")
         await asyncio.Future()
